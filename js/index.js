70d91d8ef5f2d25b647766f855cd7b57c7b0c72d
altizane <zane.chang@outlook.com>
2023-01-01 22:02:35 +0900

    Rendering rotation of dome

--- js/index.js
+++ js/index.js
@@ -13,7 +13,6 @@ document.body.appendChild( renderer.domElement );
 
 var createDomeAt = function( x, z, rPer, r ) {
     r = r === undefined ? 0.5 : r;
-    // mesh
     var mesh = new THREE.Mesh(
         // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
         new THREE.SphereGeometry( r, 30, 30, 0, Math.PI * 2, 0, Math.PI * 0.5 ),
@@ -28,19 +27,13 @@ var createDomeAt = function( x, z, rPer, r ) {
     mesh.geometry.rotateX( Math.PI * 2 * rPer );
     return mesh;
 };
-scene.add( createDomeAt( 0, 0, 0.0 ) );
-scene.add( createDomeAt( 0, 1, 0.5 ) );
+const sphere = createDomeAt( 0, 0, 0.0 )
+scene.add( sphere );
 renderer.render(scene, camera);
 
-// const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-// const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-// const sphere = new THREE.Mesh( geometry, material );
-// scene.add( sphere );
-// camera.position.z = 5;
-
-// function animate() {
-//     requestAnimationFrame( animate );
-//     sphere.rotation.x += 0.01; sphere.rotation.y += 0.01;
-//     renderer.render( scene, camera );
-// }
-// animate();
+function animate() {
+    requestAnimationFrame( animate );
+    sphere.rotation.x += 0.01; sphere.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+animate();
